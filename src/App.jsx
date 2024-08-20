a2d04b02d67258759cb6345fe6e3e4a89bae3893
YogeshRaghav <yogeshraghav986@gmail.com>
2024-08-20 15:34:59 +0530

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import "./App.css";
 import Main from "./components/Main";
-import SearchMenu from './mui/SearchMenu'
+import { Route, Routes } from "react-router-dom";
+
+// import SearchMenu from './mui/SearchMenu'
 
 function App() {
   return (
     <div className="app">
       {/* <SearchMenu/>   */}
-      <Main/>
+      <Routes>
+        <Route path = '/' element = {<Main/>}> </Route>
+      </Routes>
+      {/* <Main/> */}
 
     </div>
   );
